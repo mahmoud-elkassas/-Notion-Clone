b04cf77c13e1a48c8a2879e739ad842ff28ac84e
mahmoud <moelkassas8@gmail.com>
2024-08-10 05:24:27 +0300

    commit

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -14,15 +14,16 @@ interface DocumentIdPageProps {
   params: {
     documentId: Id<"documents">;
   };
-};
+}
 
-const DocumentIdPage = ({
-  params
-}: DocumentIdPageProps) => {
-  const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }) ,[]);
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    []
+  );
 
   const document = useQuery(api.documents.getById, {
-    documentId: params.documentId
+    documentId: params.documentId,
   });
 
   const update = useMutation(api.documents.update);
@@ -30,7 +31,7 @@ const DocumentIdPage = ({
   const onChange = (content: string) => {
     update({
       id: params.documentId,
-      content
+      content,
     });
   };
 
@@ -51,22 +52,23 @@ const DocumentIdPage = ({
   }
 
   if (document === null) {
-    return <div>Not found</div>
+    return <div>Not found</div>;
   }
 
-  return ( 
+  return (
     <div className="pb-40">
       <Cover preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar preview initialData={document} />
         <Editor
-          editable={false}
-          onChange={onChange}
           initialContent={document.content}
+          onChange={function (value: string): void {
+            throw new Error("Function not implemented.");
+          }}
         />
       </div>
     </div>
   );
-}
- 
+};
+
 export default DocumentIdPage;
